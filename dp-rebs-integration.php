c7624ddfe9dc043bf1f592e48f776bd4d90d3008
Dan Ștefancu <dan@dreamproduction.ro>
2015-03-18 18:17:36 +0200

    Main file with description, git stuff

---
+++ dp-rebs-integration.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+Plugin Name: DP REBS Integration
+Plugin URI: http://dreamproduction.com/wordpress-plugins/dp-rebs-integration
+Description: Get properties and values from REBS API.
+Version: 1.0
+Author: Dream Production
+Author URI: http://dreamproduction.com
+License: GPL2
+*/
+
+class DP_REBS extends DP_Plugin {
+
+}
