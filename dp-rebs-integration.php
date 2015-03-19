7a3e0b91c54dc7e77960715896402d9e3cfd4e66
Dan Ștefancu <dan@dreamproduction.ro>
2015-03-19 16:19:39 +0200

    Added calls for main plugin method.

--- dp-rebs-integration.php
+++ dp-rebs-integration.php
@@ -10,6 +10,26 @@ Author URI: http://dreamproduction.com
 License: GPL2
 */
 
-class DP_REBS extends DP_Plugin {
+include( 'inc/dp-plugin/dp-plugin.php' );
 
+class DP_REBS extends DP_Plugin {
+	/**
+	 * Main plugin method. Called right after auto-loader.
+	 */
+	function plugin_init() {
+		// nothing here yet
+	}
 }
+
+/**
+ * Instantiate the plugin class if not already done so.
+ *
+ * @return DP_REBS
+ */
+function dp_rebs() {
+	$instance_object = apply_filters( 'dp_rebs_integration_object', 'DP_REBS' );
+	return $instance_object::instance();
+}
+
+// fire plugin after theme
+add_action( 'after_setup_theme', 'dp_rebs', 5 );
