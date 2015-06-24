268ae6fb90c262f6c1718b70dbfc7c431ba913e4
Dan Ștefancu <dan@dreamproduction.ro>
2015-06-24 13:30:54 +0300

    Debug log for features terms.

--- class-dp-rebs-property.php
+++ class-dp-rebs-property.php
@@ -392,21 +392,32 @@ class DP_REBS_Property {
 					$to_insert = array();
 					foreach ( $terms as $parent => $term_array ) {
 						if ( !$parent_result = term_exists($parent, $taxonomy) ) {
+							$this->log( 'tax: insert parent slug:' . $parent );
 							$parent_result = wp_insert_term( $parent, $taxonomy);
 						}
 
+
 						$to_insert[] = $parent_id = $parent_result['term_id'];
 
+						$this->log( 'tax: parent ok. slug:' . $parent . ' id:' . $parent_id );
+
+
 						foreach ( $term_array as $term ) {
+
+							$this->log( 'tax: check child term slug:' . $term );
 
 							// try slug-parent-slug first, as many terms are used under multiple parents
 							if ( $alt_result = term_exists( $term, $taxonomy, $parent_id ) ) {
 								// slug-parent-slug
 								$term_to_insert = $alt_result['term_id'];
+
+								$this->log( 'tax: child term exists:' . $term . ' id: ' . $term_to_insert );
 							} else {
 								// verify if exists, add it if not
 								$result = wp_insert_term( $term, $taxonomy, array( 'parent' => $parent_id ));
 								$term_to_insert = $result['term_id'];
+
+								$this->log( 'tax: child term inserted:' . $term . ' id: ' . $term_to_insert );
 							}
 
 							$to_insert[] = $term_to_insert;
